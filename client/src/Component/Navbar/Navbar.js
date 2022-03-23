c09fb2c31a6382a81cc4362b707a1abb302dadd0
smkamaldar <smkamaldar@gmail.com>
2022-03-23 21:41:24 +0000

    fixing import

--- client/src/Component/Navbar/Navbar.js
+++ client/src/Component/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import {
     NavMenu,
     NavBtn,
     NavBtnLink,
-  } from "./NavbarElements";
+  } from "./NavBarElements";
 import "../../pages/Home.css";
 import image from "../../assets/Siriuslogo.png";
 
