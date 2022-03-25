f7446d72ac73be8ea407623d637e77057c78d4b3
teejad <hibamabati@gmail.com>
2022-03-25 15:54:50 +0000

    troubleshooting

--- client/src/pages/AddEntry.js
+++ client/src/pages/AddEntry.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -19,26 +18,25 @@ const [competence, setCompetence] = useState("");
 
 const submitEntry = () => {
 
- fetch("/api/stars"), {
-    method: "POST",
-    Headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
+  fetch("/api/stars",
+  {
+    method:"POST",
+    headers: { 'Content-Type' : 'application/json'},
+    body : JSON.stringify({
       title,
+      competence,
       situation,
       task,
       action,
-      result,
-      competence,
-    }),
-  }
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      navigate("/viewEntries");
+      result
     })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  })
+  .then((response)=> response.json())
+  .then((data)=>{
+    console.log(data)
+    // navigate("/viewentries")
+  })
 };
 
 return (
