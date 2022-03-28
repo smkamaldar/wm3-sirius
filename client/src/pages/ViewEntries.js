7524234f4d73d4be13c922fb5a5082da7a10d326
smkamaldar <smkamaldar@gmail.com>
2022-03-28 16:10:58 +0100

    fetching stars and show it on viewEntries page

--- client/src/pages/ViewEntries.js
+++ client/src/pages/ViewEntries.js
@@ -1,60 +1,66 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12];
+import React, { useEffect, useState } from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { getImageByName } from "../utils/image.js";
+
 
 const theme = createTheme();
 
 export default function ViewEntries() {
-    return (
-      <ThemeProvider theme={theme}>
+	const [stars, setStars] = useState([]);
 
-        <main>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={6}>
-              <Card
-                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-              >
-                <CardMedia
-                  component="img"
-                  sx={{
-                    height: '50%',
-                    // 16:9
-                    // pt: '56.25%',
-                  }}
-                  image="https://images.unsplash.com/photo-1538045698727-ac45d4365100?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
-                  alt="random"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    STAR
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the
-                    content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Share</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </main>
-    </ThemeProvider>
-    );
-    }
-
-
+	useEffect(() => {
+		fetch("/api/stars")
+			.then((response) => response.json())
+			.then((data) => setStars(data));
+	}, []);
+	return (
+		<ThemeProvider theme={theme}>
+			<main>
+				<Container sx={{ py: 8 }} maxWidth="md">
+					<Grid container spacing={4}>
+						{stars.map((star) => (
+							<Grid item key={star.id} xs={12} sm={6} md={6}>
+								<Card
+									sx={{
+										height: "100%",
+										display: "flex",
+										flexDirection: "column",
+									}}
+								>
+									<CardMedia
+										component="img"
+										sx={{
+											height: "50%",
+											// 16:9
+											// pt: '56.25%',
+										}}
+										image={getImageByName(star.image)}
+										alt="random"
+									/>
+									<CardContent sx={{ flexGrow: 1 }}>
+										<Typography gutterBottom variant="h5" component="h2">
+											{star.title}
+										</Typography>
+										<Typography>{star.competence}</Typography>
+									</CardContent>
+									<CardActions>
+										<Button size="small">View</Button>
+										<Button size="small">Share</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+			</main>
+		</ThemeProvider>
+	);
+}
