31cdaaf7dae4e6c749422ce76d18b9bdc71dca54
smkamaldar <smkamaldar@gmail.com>
2022-03-28 16:04:35 +0100

    making images folder & creating a function to import image

---
+++ client/src/utils/image.js
@@ -0,0 +1,29 @@
+import image1 from "../assets/images/1.png";
+import image2 from "../assets/images/2.png";
+import image3 from "../assets/images/3.png";
+import image4 from "../assets/images/4.png";
+import image5 from "../assets/images/5.png";
+import image6 from "../assets/images/6.png";
+import image7 from "../assets/images/7.png";
+import image8 from "../assets/images/8.png";
+import image9 from "../assets/images/9.png";
+import image10 from "../assets/images/10.png";
+import image11 from "../assets/images/11.png";
+
+export const getImageByName = (name)=>{
+    const images = {
+        "1.png": image1,
+        "2.png": image2,
+        "3.png": image3,
+        "4.png": image4,
+        "5.png": image5,
+        "6.png": image6,
+        "7.png": image7,
+        "8.png": image8,
+        "9.png": image9,
+        "10.png": image10,
+        "11.png": image11,
+    }
+    return images[name]
+}
+
