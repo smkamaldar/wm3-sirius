2abbee368d08f1f2406505e68c7adecce903faeb
teejad <hibamabati@gmail.com>
2022-03-28 21:09:22 +0100

    Fixed some errors in the user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,43 +10,34 @@ const bcrypt = require("bcrypt");
 
 
 
-router.get("/", async (req, res) => 
+router.get("/", async (req, res) =>
 {
 	res.send("users info")
-})
+});
 
 
-router.post("/register", async (req, res) =>
- {
-	res.send("register new user");
-})
 
-
-router.post('/users/register', async  (req, res) => 
-{ 
-	const { first_name, last_name, email, password} = req.body;
+router.post('/register', async  (req, res) => {
+	const { first_name, last_name, email, password } = req.body;
 
 let errors = [];
 
-if (!first_name||last_name||!email||!password ||!password2) 
-{ 
-	error.push({msg: 'please enter all fields'});
+if (!first_name||last_name||!email||!password ){
+
+	errors.push({msg: 'please enter all fields'});
 	}
 
-if(password !== password2){
-	errors.push({
-	msg: 'password do not match'});
-	}
 
 if(password.length < 8 ){
-	error.push({msg: 'password should be at least 8 characters'});
+	errors.push({msg: 'password should be at least 8 characters'});
 	}
 
 if(errors.length > 0) {
-	res.render('register', { errors, first_name, last_name, email, password, password2,});
+
+	res.render('register', { errors, first_name, last_name, email, password });
 	} else {
-		
-let hashedPassword  = await bcrypt.hash(password, 10);
+
+const hashedPassword  = await bcrypt.hash(password, 10);
 
 
 Pool.query (
@@ -57,26 +48,23 @@ Pool.query (
 	console.log (results.rows);
 
 	if(results.rows.length > 0){
-		errors.push({msg: "Email already registered"});
-		res.render('register', {errors});
+		errors.push({ msg: "Email already registered" });
+		res.render('register', { errors });
 	}else {
 			Pool. query (
-				`INSERT INTO user (first_name, last_name, email, password) VALUE($1, $2, $3, $4)RETURNING id, password`, [first_name, last_name, email, hashedPassword], (error, results) =>
-				{
+				`INSERT INTO user (first_name, last_name, email, password) VALUE($1, $2, $3, $4)RETURNING id, password`, [first_name, last_name, email, hashedPassword], (error, results) =>{
 					if (error){
 						throw error;
 					}
-			
-			console.log(result.rows);
-				req.flash('success_msg', "You are now registered. Please log in", );
-				res.redirect("/users/login")
+
+			console.log(results.rows);
+				req.send('success_msg', "You are now registered. Please log in", );
+				res.redirect("/users/login");
+				});
 				}
-				);
-				}
-				}
-				);
-				}
-})
+				});
+			}
+});
 
 
 export default router;
