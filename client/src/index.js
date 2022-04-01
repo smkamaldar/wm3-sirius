d8a4f8a78ab9da5ae06f2e67a60d1981c0231e9c
teejad <hibamabati@gmail.com>
2022-04-01 09:30:11 +0100

    Context Configutation added to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
+import { AuthProvider } from "././context/AuthProvider"
 import App from "./App";
 
 ReactDOM.render(
 	<BrowserRouter>
+		<AuthProvider>
 		<App />
+		</AuthProvider>
 	</BrowserRouter>,
 	document.getElementById("root")
 );
