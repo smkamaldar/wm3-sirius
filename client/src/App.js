4867efd2615127480599a29d4efc6215c31b610d
gempearl <nonyeagwunobi@gmail.com>
2022-04-04 15:31:50 +0100

    assigned signin page as main page

--- client/src/App.js
+++ client/src/App.js
@@ -17,11 +17,9 @@ const App = () => (
 		<Navbar />
 		<Box className="body">
 			<Routes>
-				<Route path="/" element={<Home />} />
 				<Route path="/" element={<SignIn />} />
 				<Route path="/addentry" element={<AddEntry />} />
-				<Route path="/viewentries" element={<ViewEntries />} />
-			
+				<Route path="/viewentries" element={<ViewEntries />} />			
 				<Route path="/SignIn" element={<SignIn />} />
 				<Route path="/SignUp" element={<SignUp />} />
 				<Route path="/About" element={<About />} />
