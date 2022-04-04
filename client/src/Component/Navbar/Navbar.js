afbcb829acd2e80847a0f2a2da7c1bb8fdf3a8c4
gempearl <nonyeagwunobi@gmail.com>
2022-04-04 16:05:21 +0100

    Log in button colour changed and reworded to Sign out

--- client/src/Component/Navbar/Navbar.js
+++ client/src/Component/Navbar/Navbar.js
@@ -5,62 +5,92 @@ import {
     NavMenu,
     NavBtn,
     NavBtnLink,
-	NavBtnLogout
-  } from "./NavBarElements";
-  import useAuth from "../../hooks/useAuth";
+    NavBtnLogout
+} from "./NavBarElements";
+import useAuth from "../../hooks/useAuth";
 import "../../pages/Home.css";
 import image from "../../assets/Siriuslogo.png";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
-	const { auth, setAuth } = useAuth();
-	const navigate = useNavigate();
-	const handleLogout = ()=>{
-		fetch("/api/auth/logout",{
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-		})
-		.then(response => response.json() )
-		.then(data => {
-			setAuth(0);
-			navigate("/signin");
-		});
-	};
-	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<NavLink to="/" className="logo">
-				<img src={image} height={100} width={100} alt="Logo" />
-			</NavLink>
-			<Bars />
-			<NavMenu className="navbar-menu">
-				<NavLink to="/" style={{ padding: "5px", color: "black" }}>
-					Home
-				</NavLink>
-				<NavLink to="/about" style={{ color: "black" }}>
-					About
-				</NavLink>
-				<NavLink
-					to="/addentry" style={{ color: "black" }}>
-					{" "}
-					Add A Star
-				</NavLink>
-				<NavLink to="/contact" style={{ color: "black" }}>
-					Contact Us
-				</NavLink>
-				<NavLink to="/signup" style={{ color: "black" }}>
-					Sign Up
-				</NavLink>
-			</NavMenu>
-			<NavBtn>
-				{auth ? <NavBtnLogout onClick={handleLogout}>log out</NavBtnLogout> : <NavBtnLink
-				style={{
-					background: "black",
-					color: "white",
-				}}
-				to="/signin">Sign In</NavBtnLink>}
-			</NavBtn>
-		</nav>
-	);
+    const {auth, setAuth} = useAuth();
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        fetch("/api/auth/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => response.json()).then((data) => {
+            setAuth(0);
+            navigate("/signin");
+        });
+    };
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink to="/" className="logo">
+                <img src={image}
+                    height={100}
+                    width={100}
+                    alt="Logo"/>
+            </NavLink>
+            <Bars/>
+            <NavMenu className="navbar-menu">
+                <NavLink to="/"
+                    style={
+                        {
+                            padding: "5px",
+                            color: "black"
+                        }
+                }>
+                    Home
+                </NavLink>
+                <NavLink to="/about"
+                    style={
+                        {color: "black"}
+                }>
+                    About
+                </NavLink>
+                <NavLink to="/addentry"
+                    style={
+                        {color: "black"}
+                }>
+                    {" "}
+                    Add A Star
+                </NavLink>
+                <NavLink to="/contact"
+                    style={
+                        {color: "black"}
+                }>
+                    Contact Us
+                </NavLink>
+                <NavLink to="/signup"
+                    style={
+						{color: "black"}
+                }>
+                    Sign Up
+                </NavLink>
+            </NavMenu>
+            <NavBtn> {
+                auth ? (
+                    <NavBtnLogout className="signOut"
+                        onClick={handleLogout}>
+                       Sign Out
+                    </NavBtnLogout>
+                ) : (
+                    <NavBtnLink style={
+                            {
+                                background: "black",
+                                color: "white"
+                            }
+                        }
+                        to="/signin">
+                        Sign In
+                    </NavBtnLink>
+                )
+            } </NavBtn>
+        </nav>
+    );
 };
 
 export default Navbar;
